8c3d060ac8d2d1462e40c08b43501cd3ec5ee55f
Omar Al-Ithawi <i@omardo.com>
2020-04-08 15:14:58 +0300

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='tahoe-lti',
-    version='0.1.0',
+    version='0.2.0',
     description='Tahoe LTI Customizations package.',
     packages=[
         'tahoe_lti',
