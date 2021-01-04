d3ca2273bfc4dd61721ced6814525206973d755d
Omar Al-Ithawi <i@omardo.com>
2021-01-04 10:13:13 +0300

    fixup

--- tahoe_lti/openedx.py
+++ tahoe_lti/openedx.py
@@ -1,10 +1,9 @@
 """
 A module to collect all Open edX imports in a single place.
 
-The module aims to list all imports explicitly until proper `api.py` is developed in Open edX for this plugin.
+The module aims to list all imports explicitly until proper `api.py` is
+developed in Open edX for this plugin.
 """
 
-from openedx.core.djangoapps.course_groups import cohorts
-
-from lms.djangoapps.teams.models import CourseTeamMembership
-
+from openedx.core.djangoapps.course_groups import cohorts  # noqa
+from lms.djangoapps.teams.models import CourseTeamMembership  # noqa
