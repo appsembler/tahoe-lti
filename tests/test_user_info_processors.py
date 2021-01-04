f23ad71d403fd5e96aa74527e4de1ae62d6c2fec
Omar Al-Ithawi <i@omardo.com>
2021-01-04 09:01:23 +0300

    tests for personal_user_info

--- tests/test_user_info_processors.py
+++ tests/test_user_info_processors.py
@@ -1,6 +1,6 @@
 from mock import patch, Mock
 
-from tahoe_lti.processors import basic_user_info
+from tahoe_lti.processors import basic_user_info, personal_user_info
 
 
 @patch('tahoe_lti.processors.get_xblock_user')
@@ -15,3 +15,26 @@ def test_basic_user_info(mock_get_xblock_user):
         'lis_person_contact_email_primary': 'mock_email@example.com',
     }
     assert not hasattr(basic_user_info, 'lti_xblock_default_params')
+
+
+@patch('tahoe_lti.processors.get_xblock_user')
+def test_personal_user_info(mock_get_xblock_user):
+    """Happy scenario for personal_user_info"""
+    assert personal_user_info.lti_xblock_default_params == {
+        'lis_person_name_full': '',
+        'lis_person_name_given': '',
+        'lis_person_name_family': '',
+        'custom_user_id': '',
+    }
+    mock_user = Mock()
+    mock_user.profile.name = 'Bob Robot'
+    mock_user.id = 20
+    mock_get_xblock_user.return_value = mock_user
+
+    info = personal_user_info(xblock=None)
+    assert info == {
+        'custom_user_id': '20',
+        'lis_person_name_full': 'Bob Robot',
+        'lis_person_name_given': 'Bob',
+        'lis_person_name_family': 'Robot',
+    }
